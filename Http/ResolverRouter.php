085a20ca3f3b9a1ecdbe520798abdfab7fbcaa52
Kristers Zarins <kristerskz@gmail.com>
2017-11-09 18:23:59 +0200

    Ajax Api for retreiving entry contents

--- Http/ResolverRouter.php
+++ Http/ResolverRouter.php
@@ -20,6 +20,12 @@ class ResolverRouter
             'namespace'  => 'Modules\Content\Http\Controllers\Client'
         ], function (Router $router) {
 
+            $router->get('/content/entry/{entry}', [
+                'uses'       => 'ApiController@entry',
+                'as'         => 'api.entry',
+                'middleware' => []
+            ]);
+
             $router->get('/{slug}', [
                 'uses'       => 'ResolverController@resolve',
                 'as'         => 'resolver',
