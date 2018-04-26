37910bdd663ef49d1f7299d9055879e42f40515e
Daniels Grietins <daniels.grietins@gmail.com>
2018-04-26 18:46:46 +0300

    Improvements and new features

--- Resources/views/module_content/entries/form/attachments.blade.php
+++ Resources/views/module_content/entries/form/attachments.blade.php
@@ -1,13 +1,14 @@
-@if(isset($channel) && $channel->allow_attachments)
+@if($channel->allow_attachments)
     <div class="panel">
         <div class="panel-heading">
-            <div class="panel-title">
-                Attachments
+            Attachments
+            <div class="pull-right">
+                <button class="btn btn-xs btn-success js-toggle-panel-body">Show/hide</button>
             </div>
         </div>
-        <div class="panel-body">
+        <div class="panel-body" style="display: none;">
             <div class="form-group">
-                <label>Attachment (You can select multiple)</label>
+                <label>Attachment <small>(You can select multiple) or select one for media cover</small></label>
                 <br>
                 {!! Form::file('attachments[]', [
                     'class' => 'form-control form-input attachment',
@@ -15,22 +16,40 @@
                 ]) !!}
 
                 <span class="error-span"></span>
-
+            </div>
+            <div class="form-group">
+                <label>Media <small>(Youtube video or iframe)</small></label>
+                <br>
+                {!! Form::text('media', null, [
+                    'class' => 'form-control',
+                ]) !!}
 
                 @if(isset($entry) && $entry->attachments->count() )
                     <br>
                     <table class="table table-bordered">
                         <thead>
                         <tr>
-                            <th>Attachment</th>
+                            <th>Attachment/Cover</th>
+                            <th>Media</th>
+                            <th style="width: 15%;">Is featured</th>
                             <th style="width: 15%">Actions</th>
                         </tr>
                         </thead>
                         <tbody>
                         @foreach($entry->attachments as $attachment)
                             <tr>
-                                <td><img src="{{ $attachment->image->url('original') }}" alt=""
-                                            style="max-width: 100px;"></td>
+                                <td>
+                                    <img src="{{ $attachment->image->url('original') }}" alt=""
+                                         style="max-width: 100px; max-height: 50px;">
+                                </td>
+                                <td>
+                                    {{ $attachment->media }}
+                                </td>
+                                <td>
+                                    <span class="hidden-switchery" hidden>
+                                        <input type="checkbox" class="switchery js-toggle-attachment-featured" data-id="{{ $attachment->id }}" value="1" {{ $attachment->is_featured ? 'checked' : '' }}>
+                                    </span>
+                                </td>
                                 <td>
                                     <a
                                             class="btn btn-xs btn-danger confirm-action"
